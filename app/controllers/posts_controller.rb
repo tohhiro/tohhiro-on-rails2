ed3b697eee5d429f50c03b6552809e0300a34e2e
tohhiro <tohhiro@gmail.com>
2026-01-03 12:22:04 +0900

    編集ページを追加し、投稿を編集できるように実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,6 +16,19 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to posts_path, notice: 'Post was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
   private
 
   def post_params
